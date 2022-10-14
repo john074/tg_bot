10dd6677e17edfafa1cbb7833c3c492b1408f4a5
Ilya Golubchikov <mydogfeel@mail.ru>
2022-10-14 19:57:29 +0700

    Initial commit

---
+++ download_audio.py
@@ -0,0 +1,46 @@
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+ydl_opts = {
+    'format': 'm4a/bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+    }],
+    'outtmpl': '1.mp3',
+}
+
+
+def download_audio(url, chat_id, bot):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    if os.path.getsize('1.mp3') > 51380224:
+        split_audio(chat_id, bot)
+    else:
+        send_audio(chat_id, bot)
+
+
+def send_audio(chat_id, bot, amount=1):
+    if amount == 1:
+        audio = open('1.mp3', 'rb')
+        bot.send_audio(chat_id, audio)
+    else:
+        audio = open('1.mp3', 'rb')
+        bot.send_audio(chat_id, audio)
+        audio = open('2.mp3', 'rb')
+        bot.send_audio(chat_id, audio)
+
+
+def split_audio(chat_id, bot):
+    sound = AudioSegment.from_mp3("1.mp3")
+    halfway_point = len(sound) / 2
+
+    first_half = sound[:halfway_point]
+    second_half = sound[halfway_point:]
+
+    first_half.export("1.mp3", format="mp3")
+    second_half.export("2.mp3", format="mp3")
+
+    send_audio(chat_id, bot, 2)
---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+import download_audio
+import threading
+
+bot = telebot.TeleBot("5504530111:AAEffGN-dgcJs8gOGdiUGvAygjF_QKxGqMw")
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Welcome to the bot\nType a link to a video to get the audio file")
+
+
+@bot.message_handler(content_types=['text'])
+def check_message(message):
+    if message.text.startswith('https://www.youtube.com/') or message.text.startswith('youtube.com/')\
+            or message.text.startswith('https://youtu.be/') or message.text.startswith('youtu.be/'):
+        bot.send_message(message.chat.id, "Processing...\nIt may take a minute")
+        threading.Thread(target=download_audio.download_audio,
+                         args=(message.text, message.chat.id, bot)).start()
+
+
+bot.infinity_polling()
