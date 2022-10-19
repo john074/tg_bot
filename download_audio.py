d1cdc18f6bab24c4f763f2aef021b075f1a30d4d
I ate Pizza <77271270+john074@users.noreply.github.com>
2022-10-19 11:00:09 +0700

    Update download_audio.py

--- download_audio.py
+++ download_audio.py
@@ -1,8 +1,8 @@
 import yt_dlp
 from pydub import AudioSegment
 import os
-import threading
 import time
+import threading
 
 ydl_opts = {
     'format': 'm4a/bestaudio/best',
@@ -10,7 +10,8 @@ ydl_opts = {
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
     }],
-    'outtmpl': '1.mp3',
+    'outtmpl': 'main.mp3',
+    'playlist_items': '1',
 }
 
 
@@ -18,42 +19,37 @@ def download_audio(url, chat_id, bot):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    if os.path.getsize('1.mp3') > 51380224:
+    if os.path.getsize('main.mp3') > 51380224:
         split_audio(chat_id, bot)
     else:
+        os.rename('main.mp3', '0.mp3')
         send_audio(chat_id, bot)
 
 
-def send_audio(chat_id, bot, amount=1):
-    if amount == 1:
-        audio = open('1.mp3', 'rb')
-        bot.send_audio(chat_id, audio)
-    else:
-        audio = open('1.mp3', 'rb')
+def send_audio(chat_id, bot, amount=0):
+    for i in range(amount):
+        audio = open(f'{i}.mp3', 'rb')
         bot.send_audio(chat_id, audio)
         audio.close()
-        audio = open('2.mp3', 'rb')
-        bot.send_audio(chat_id, audio)
-        audio.close()
-
-    threading.Thread(target=clean_up).start()
+    threading.Thread(clean_up(amount)).start()
 
 
 def split_audio(chat_id, bot):
-    sound = AudioSegment.from_mp3("1.mp3")
-    halfway_point = len(sound) / 2
+    sound = AudioSegment.from_mp3('main.mp3')
+    parts = os.path.getsize('main.mp3') // 51380224 + 1
+    break_point = len(sound) / parts
+    for i in range(parts):
+        sound[i * break_point:(i + 1) * break_point].export(f'{i}.mp3', format='mp3')
 
-    first_half = sound[:halfway_point]
-    second_half = sound[halfway_point:]
-
-    first_half.export("1.mp3", format="mp3")
-    second_half.export("2.mp3", format="mp3")
-
-    send_audio(chat_id, bot, 2)
+    send_audio(chat_id, bot, parts)
 
 
-def clean_up():
+def clean_up(amount):
     time.sleep(10)
-    os.remove('1.mp3')
-    if os.path.exists('2.mp3'):
-        os.remove('2.mp3')
+    try:
+        for i in range(amount + 1):
+            if os.path.exists(f'{i}.mp3'):
+                os.remove(f'{i}.mp3')
+        os.remove('main.mp3')
+    except FileNotFoundError:
+        pass
